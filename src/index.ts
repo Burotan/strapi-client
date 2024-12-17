b4fa945f473afa52cdb962c8307b2ad45a1725db
Burak Serpici <bserpici@gmail.com>
2024-12-17 13:17:31 +0300

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { StrapiClient as Strapi } from "./client/StrapiClient";
+import { StrapiResult, StrapiError, StrapiMeta } from "./types/StrapiOptions";
+import { StrapiQuery } from "./types/StrapiQuery";
 
-export { Strapi };
+export { Strapi, StrapiError, StrapiResult, StrapiMeta, StrapiQuery };
